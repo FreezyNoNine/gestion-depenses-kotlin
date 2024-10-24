772876bb0167ec733b5bfd95b391c694b3505107
FreezyNoNine <rachedi.jibril@yahoo.com>
2024-10-24 12:04:09 +0200

    Création de l'interface, quelques erreurs

--- app/src/main/java/com/example/gestiondepenses/MainActivity.kt
+++ app/src/main/java/com/example/gestiondepenses/MainActivity.kt
@@ -3,11 +3,9 @@ package com.example.gestiondepenses
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.* // Assure-toi d'importer tout le package material3
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,32 +14,36 @@ import com.example.gestiondepenses.ui.theme.GestionDepensesTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             GestionDepensesTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainScreen() {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            topBar = {
+                TopAppBar(title = { Text("Gestion de Dépenses") })
+            }
+        ) { innerPadding ->
+            // Contenu principal ici
+            // Par exemple, un texte d'accueil
+            Text(
+                text = "Bienvenue dans l'application de gestion de dépenses",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GestionDepensesTheme {
-        Greeting("Android")
+    @Preview(showBackground = true)
+    @Composable
+    fun PreviewMainScreen() {
+        GestionDepensesTheme {
+            MainScreen()
+        }
     }
 }
